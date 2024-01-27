46ac1283243f9907de563aa33d201e7bde003bd7
Richard Clubb <rich.clubb@gmail.com>
2024-01-27 10:15:56 +0000

    Implemented part a in classes rather than imperative

--- 2023/day05/python/day5_classes.py
+++ 2023/day05/python/day5_classes.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 from collections import defaultdict
 from enum import Enum
@@ -14,7 +15,7 @@ class SeedType(Enum):
 class Seed:
     def __init__(self, seed_start, seed_size=None):
         self.__seed_start = seed_start
-        self.__seed_end = seed_start + self.seed_size - 1
+        self.__seed_end = seed_size if seed_size is None else seed_start + seed_size - 1
         self.__seed_type = (
             SeedType.SEED_SINGLE if seed_size is None else SeedType.SEED_RANGE
         )
@@ -23,9 +24,9 @@ class Seed:
         return self.__seed_type
 
     def value(self):
-        if self.seed_type == SeedType.SEED_SINGLE:
+        if self.__seed_type == SeedType.SEED_SINGLE:
             return self.__seed_start
-        elif self.seed_type == SeedType.SEED_RANGE:
+        elif self.__seed_type == SeedType.SEED_RANGE:
             return (self.__seed_start, self.__seed_end)
         else:
             raise Exception("Cannot return value for seed type")
@@ -33,6 +34,77 @@ class Seed:
     def set_value(self, seed_start, seed_end=None):
         self.__seed_start = seed_start
         self.__seed_end = seed_end
+
+    def __eq__(self, seed):
+        if self.__seed_type == SeedType.SEED_SINGLE:
+            if self.__seed_start == seed.value:
+                return True
+            return False
+        elif self.__seed_type == SeedType.SEED_RANGE:
+            raise Exception("Not yet implemented")
+
+    def __ne__(self, seed):
+        if self.__seed_type == SeedType.SEED_SINGLE:
+            if self.__seed_start != seed.value:
+                return True
+            return False
+        elif self.__seed_type == SeedType.SEED_RANGE:
+            raise Exception("Not yet implemented")
+
+    def __lt__(self, seed):
+        if self.__seed_type == SeedType.SEED_SINGLE:
+            if self.__seed_start < seed.value():
+                return True
+            return False
+        elif self.__seed_type == SeedType.SEED_RANGE:
+            raise Exception("Not yet implemented")
+
+    def __gt__(self, seed):
+        if self.__seed_type == SeedType.SEED_SINGLE:
+            if self.__seed_start > seed.value:
+                return True
+            return False
+        elif self.__seed_type == SeedType.SEED_RANGE:
+            raise Exception("Not yet implemented")
+
+    def __le__(self, seed):
+        if self.__seed_type == SeedType.SEED_SINGLE:
+            if self.__seed_start <= seed.value:
+                return True
+            return False
+        elif self.__seed_type == SeedType.SEED_RANGE:
+            raise Exception("Not yet implemented")
+
+    def __ge__(self, seed):
+        if self.__seed_type == SeedType.SEED_SINGLE:
+            if self.__seed_start >= seed.value:
+                return True
+            return False
+        elif self.__seed_type == SeedType.SEED_RANGE:
+            raise Exception("Not yet implemented")
+
+class Seeds:
+    def __init__(self, path = None, seed_type = None):
+        self.seeds = []
+        self.index = 0
+        if path is not None:
+            self.load_seeds_from_file(path, seed_type)
+
+    def load_seeds_from_file(self, path, seed_type):
+        with open(path) as f:
+            for line in f:
+                line = line.strip()
+                if line.startswith("seeds"):
+                    seeds_raw = list(map(int, line.split()[1:]))
+                    if seed_type == SeedType.SEED_SINGLE:
+                        self.seeds = [Seed(seed) for seed in seeds_raw]
+                    elif seed_type == SeedType.SEED_RANGE:
+                        raise Exception("Not yet implemented")
+                    break
+
+    def __iter__(self):
+        for each in self.seeds:
+              yield each
 
 
 class SeedMapping:
@@ -42,11 +114,11 @@ class SeedMapping:
         self.__size = size
 
     def translate_seed(self, seed):
-        if seed.seed_type == SeedType.SEED_SINGLE:
+        if seed.seed_type() == SeedType.SEED_SINGLE:
             if (seed.value() >= self.__start) and (
                 seed.value() <= (self.__start + self.__size - 1)
             ):
-                seed.set_value(self.__dest + (seed.value() - self.start))
+                seed.set_value(self.__dest + (seed.value() - self.__start))
 
     def translate_seed_range(self, seed_range):
         raise Exception("Not yet implemented")
@@ -68,51 +140,67 @@ class MappingLayer:
 
     def translate_seed_range(self, seed_range):
         raise Exception("Not yet implemented")
+    
+class MappingLayers:
+    def __init__(self, path = None):
+        self.mapping_layers = defaultdict(MappingLayer)
+        if path is not None:
+            self.load_mappings_layers_from_file(path)
 
 
-def extract_maps_and_seeds(input_file_path, seed_type):
-    maps = defaultdict(list)
+    def load_mappings_layers_from_file(self, path):
 
-    with open(input_file_path) as f:
-        for line in f:
-            line = line.strip()
-            if line.startswith("seeds"):
-                seeds_raw = list(map(int, line.split()[1:]))
-                if seed_type == SeedType.SEED_SINGLE:
-                    seeds = [Seed(seed) for seed in seeds_raw]
-                elif seed_type == SeedType.SEED_RANGE:
-                    raise Exception("Not yet implemented")
+        with open(path) as f:
+            for line in f:
+                line = line.strip()
+                if line.startswith("seeds"):
+                    continue
 
-            elif line.startswith("seed-to-soil"):
-                active_map = 0
-                maps[active_map] = MappingLayer()
-            elif line.startswith("soil-to-fertilizer"):
-                active_map = 1
-                maps[active_map] = MappingLayer()
-            elif line.startswith("fertilizer-to-water"):
-                active_map = 2
-                maps[active_map] = MappingLayer()
-            elif line.startswith("water-to-light"):
-                active_map = 3
-                maps[active_map] = MappingLayer()
-            elif line.startswith("light-to-temperature"):
-                active_map = 4
-                maps[active_map] = MappingLayer()
-            elif line.startswith("temperature-to-humidity"):
-                active_map = 5
-                maps[active_map] = MappingLayer()
-            elif line.startswith("humidity-to-location"):
-                active_map = 6
-                maps[active_map] = MappingLayer()
-            elif line:
-                maps[active_map].add_seed_map(list(map(int, line.split())))
+                elif line.startswith("seed-to-soil"):
+                    active_map = 0
+                    self.mapping_layers[active_map] = MappingLayer()
+                elif line.startswith("soil-to-fertilizer"):
+                    active_map = 1
+                    self.mapping_layers[active_map] = MappingLayer()
+                elif line.startswith("fertilizer-to-water"):
+                    active_map = 2
+                    self.mapping_layers[active_map] = MappingLayer()
+                elif line.startswith("water-to-light"):
+                    active_map = 3
+                    self.mapping_layers[active_map] = MappingLayer()
+                elif line.startswith("light-to-temperature"):
+                    active_map = 4
+                    self.mapping_layers[active_map] = MappingLayer()
+                elif line.startswith("temperature-to-humidity"):
+                    active_map = 5
+                    self.mapping_layers[active_map] = MappingLayer()
+                elif line.startswith("humidity-to-location"):
+                    active_map = 6
+                    self.mapping_layers[active_map] = MappingLayer()
+                elif line:
+                    dest, start, size = list(map(int, line.split()))
+                    self.mapping_layers[active_map].add_seed_map(SeedMapping(dest, start, size))
+    
+    def translate_seed(self, seed):
 
-    return maps, seeds
+        for index in range(0, len(self.mapping_layers.keys())):
+            self.mapping_layers[index].translate_seed(seed)
 
 
 def part_a(path):
-    pass
+    seeds = Seeds(path, SeedType.SEED_SINGLE)
 
+    mapping_layers = MappingLayers(path)
+
+    min_seed = Seed(10**30)
+
+    for seed in seeds:
+        mapping_layers.translate_seed(seed)
+
+        if seed < min_seed:
+            min_seed = seed
+
+    return seed.value()
 
 def part_b_forwards(path):
     pass
