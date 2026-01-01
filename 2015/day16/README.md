18dc36064cfd109a5bc18bb97e6ade2d759e2146
Rich Clubb <rich.clubb@gmail.com>
2026-01-01 23:36:25 +0000

    template for 2015 day 16

---
+++ 2015/day16/README.md
@@ -0,0 +1,20 @@
+# AoC 2015 - Day 16
+
+https://adventofcode.com/2015/day/16
+
+# Status
+
+* Part A - Incomplete
+* Part B - Incomplete
+
+# Build / Run
+
+## Build
+
+[build / run instructions]
+
+# Notes
+
+
+# Improvements
+
---
+++ 2015/day16/rust/src/part_a/mod.rs
@@ -0,0 +1,30 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+pub fn part_a(path: &String)
+{
+    println!("Part A");
+
+    let file: File = File::open(path).expect("Could not open file");
+    let buf_reader:BufReader<File> = BufReader::new(file);
+
+    let file_total = buf_reader.lines().fold(0, |file_acc: i64, line| {
+            file_acc + match line {
+                    Ok(x) => {
+                    x.chars().fold(0, |line_acc: i64, b: char| 
+                        {
+                            line_acc + match b {
+                                '(' => 1,
+                                ')' => -1,
+                                _ => 0,
+                            }
+                        }
+                    )
+                },
+                Err(x) => 0,
+            }
+        }
+    );
+
+    println!("Total: {}", file_total);
+}
