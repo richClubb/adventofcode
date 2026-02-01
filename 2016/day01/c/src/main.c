358c88cb8a484ef355089ada55360fadb459e08e
Rich Clubb <rich.clubb@gmail.com>
2026-02-01 23:25:29 +0000

    Added in 2016 day1 boilerplate and C template

--- 2016/day01/c/src/main.c
+++ 2016/day01/c/src/main.c
@@ -1,12 +1,36 @@
 #include <stdio.h>
 
+#include "config.h"
 
-void main(int argc, char** argv) {
+#include "part_a.h"
+
+int main(int argc, char** argv) {
 
     printf("Advent of Code 2016 - Day 01\n");
 
+    CONFIG config;
 
+    if(load_config(&config, argc, argv))
+    {
+        return 1;
+    }
 
+    switch(config.run_type)
+    {
+        case PART_A:
+            printf("Running Part A\n");
+
+            printf("Result is: %lu\n", part_a(&config));
+            break;
+        // case PART_B:
+        //     printf("Running Part B\n");
+
+        //     printf("Result is: %lu\n", part_b(&config));
+        //     break;
+        default:
+            printf("Unsupported run type");
+            break;
+    }
     
-    return;
+    return 0;
 }
---
+++ 2016/day01/c/src/part_a/part_a.c
@@ -0,0 +1,29 @@
+#include "part_a.h"
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <limits.h>
+
+#include "config.h"
+
+
+
+typedef struct instruction_t {
+    char direction;
+    uint8_t distance;
+} INSTRUCTION;
+
+
+void injest_file(char *file_path, INSTRUCTION* instructions) {
+
+
+
+}
+
+uint64_t part_a(const CONFIG *config)
+{
+
+`
+
+    return 0;
+}
