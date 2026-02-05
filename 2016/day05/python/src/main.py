193817ec7c6cb4e3ddf5fa46ddb0f45b18a35325
Rich Clubb <rich.clubb@gmail.com>
2026-02-05 23:21:05 +0000

    more poetry messing

--- 2016/day05/python/src/main.py
+++ 2016/day05/python/src/main.py
@@ -1,19 +1,13 @@
-
 #!/bin/env python3
 
-import argparse
+from part_a import run
+
+def main():
+
+    print("Advent of code 2016 - Day 06")
+
+    run()
 
 if __name__ == "__main__":
     pass
-
-    parser = argparse.ArgumentParser(
-        prog='day05',
-        description='Advent of code 2016 Day 06',
-        epilog='Merry Christmas (in February)'
-    )
-
-    parser.add_argument("-i", "--input")
-    parser.add_argument("-r", "--run")
-
-    args = parser.parse_args()
-    print(args.input, args.run)
+    
